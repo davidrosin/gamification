d35a31d0fb6a8933a9958b8c0c7087ca9ba51e10
davidrosin <38048691+davidrosin@users.noreply.github.com>
2025-11-27 08:22:05 -0300

    Create Next.js

---
+++ Next.js
@@ -0,0 +1,4 @@
+npx create-next-app@latest mission-cumprida --typescript --tailwind --eslint --app --src-dir
+cd mission-cumprida
+npm install @supabase/supabase-js lucide-react
+npm install -D @types/node
