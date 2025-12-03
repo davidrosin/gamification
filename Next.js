83264ed3e3b77a32d7bfd79f18a07599e5425597
davidrosin <38048691+davidrosin@users.noreply.github.com>
2025-12-03 10:43:38 -0300

    Delete Next.js

--- Next.js
+++
@@ -1,4 +0,0 @@
-npx create-next-app@latest mission-cumprida --typescript --tailwind --eslint --app --src-dir
-cd mission-cumprida
-npm install @supabase/supabase-js lucide-react
-npm install -D @types/node
